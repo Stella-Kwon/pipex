455ce2f99b65167e452cdd33e5b72754b7dc50e3
skwon2 <skwon2@c1r6p8.hive.fi>
2024-03-07 16:04:08 +0200

    pipex.c

--- pipex.c
+++ pipex.c
@@ -3,10 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: suminkwon <suminkwon@student.42.fr>        +#+  +:+       +#+        */
+/*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/01 14:32:52 by suminkwon         #+#    #+#             */
-/*   Updated: 2024/03/01 14:32:53 by suminkwon        ###   ########.fr       */
+/*   Updated: 2024/03/07 15:22:42 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+int main(int argc, char **argv, char **env)
+{
+    int fd[2];
+    int pid;
+    t_pipe data;
+    
+    if (pipe(fd) == -1)
+    {
+        perror("Failed to generate pipe");
+        return (0);
+    }
+    
+    while (pid)
+    {
+        
+        if (pid == 0)
+        {
+            child process // need to end.
+            if ()
+            {
+                
+            }
+        }
+        else
+        {
+            fork();
+        }
+    }
+    return (0);
+}
