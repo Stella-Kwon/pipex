6ee4d3dfc06372156a24327f1d3f4a053f78172d
Stella-Kwon <60865769+Stella-Kwon@users.noreply.github.com>
2024-04-15 10:33:36 +0300

    libft/

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suminkwon <suminkwon@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 11:52:35 by sukwon            #+#    #+#             */
+/*   Updated: 2024/04/08 14:41:53 by suminkwon        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	// ft_putstr_fd((char *)s1, 2);
+	// ft_putstr_fd((char *)s2, 2);
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if ((unsigned char)s1[i] != (unsigned char)s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
+}
